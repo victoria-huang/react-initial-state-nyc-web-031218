ae7aa593178afb01fba1a291703f1b7a511f92ef
victoria-huang <vctrah@gmail.com>
2018-05-15 09:30:34 -0400

    Done.

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,1 +1,10 @@
 // Component Code Goes Here
+import React from 'react';
+
+export default class Address extends React.Component {
+  render() {
+    return (
+      <div className="address">{this.props.street}, {this.props.city}</div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Address from './components/Address';
+import ToggleButton from './components/ToggleButton';
+
 ReactDOM.render(
-  <div>Components Need To Be Mounted</div>,
+  <div>
+    <Address
+       street="Santa Monica Blvd."
+       city="Santa Monica"
+     />
+    <ToggleButton />
+  </div>,
   document.getElementById('root')
 )
